6de51798f1ceb8e00b41d3918a4faeac7c2c8c9b
Heber Nieto <chichonet.14@gmail.com>
2023-09-11 18:01:27 -0300

    Header listo

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,76 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Header from "./Header"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <Header />
+      <div className="mt-24 ml-4">
+        <ul>
+          <li className="flex">
+            <img className="rounded-full h-7 w-7" src="/img/Heber.jpg"></img>
+            <p className="ml-4">Heber Punk</p>
+          </li>
+          <li className="flex">
+            <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+            <p className="ml-4">Amigos</p>
+          </li>
+          <li className="flex">
+            <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+            <p className="ml-4">Feeds</p>
+          </li>
+          <li className="flex">
+            <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+            <p className="ml-4">Grupos</p>
+          </li>
+          <li className="flex">
+            <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+            <p className="ml-4">Marketplace</p>
+          </li>
+          <li className="flex">
+            <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+            <p className="ml-4">Video</p>
+          </li>
+          <li className="flex">
+            <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+            <p className="ml-4">Recuerdos</p>
+          </li>
+          <li className="flex">
+            <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+            <p className="ml-4">Ver más</p>
+          </li>
+        </ul>
+        <hr className="mt-8 w-64"></hr>
+        <p>Tus accesos directos</p>
+        <ul className="">
+          <li className="flex">
+              <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+              <p className="ml-4">Heber Punk</p>
+          </li>
+          <li className="flex">
+              <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+              <p className="ml-4">El forrex</p>
+          </li>
+          <li className="flex">
+              <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+              <p className="ml-4">Best Fiends</p>
+          </li>
+          <li className="flex">
+              <image><svg fill="#fff" width="26px" height="26px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M80.8806,58.106A17.969,17.969,0,1,0,50.7979,38.4587a30.0584,30.0584,0,0,0-5.9173-4.3527,18,18,0,1,0-29.7612,0A29.8957,29.8957,0,0,0,0,60a5.9966,5.9966,0,0,0,6,6H42.1494A29.7165,29.7165,0,0,0,36,84a5.9966,5.9966,0,0,0,6,6H90a5.9966,5.9966,0,0,0,6-6A29.8957,29.8957,0,0,0,80.8806,58.106ZM66,42a6,6,0,1,1-6,6A6.0116,6.0116,0,0,1,66,42ZM24,24a6,6,0,1,1,6,6A6.0116,6.0116,0,0,1,24,24Zm6,18A18.0182,18.0182,0,0,1,46.9688,54H13.0313A18.018,18.018,0,0,1,30,42ZM49.0313,78a17.9974,17.9974,0,0,1,33.9375,0Z"/></svg></image>
+              <p className="ml-4">Candy Crush Saga</p>
+          </li>
+        </ul>
+        <div className="mt-24">
+          <p className="text-sm">Privacidad · Condiciones · Publicidad ·</p>
+          <p className="text-sm"> Opciones de anuncios · Cookies · Meta </p>
+          <p className="text-sm">© 2023</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+     
     </main>
-  )
+
+  );
 }
+
+
